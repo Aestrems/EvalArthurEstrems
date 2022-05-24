4ce2efb15f29e0746376cb43cd594aa01924961e
Arthur Estrems <arthurjeu7@gmail.com>
2022-05-24 12:06:39 +0200

    Exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Statistique } from './models/statistique';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'EvalArthurEstrems';
+export class AppComponent { 
+  title = 'EvalArthurEstrems'; 
+
+  listeStatistique: Statistique[];
+
+  nombreEnfantPourcent: Statistique = {
+    id: "fa1f5f40-be3b-11eb-91ec-7f5875ecfb46",
+    titre: "Pourcentage d'enfant",
+    valeur: "60%"
+  }
+
+  nombreAdultePourcent: Statistique = {
+    id: "fa1e6f59-be3b-11eb-91ec-9g3286ebfc46",
+    titre: "Pourcentage d'adulte",
+    valeur: "40%"
+  }
+
+  constructor() {
+    this.listeStatistique = [this.nombreAdultePourcent, this.nombreEnfantPourcent];
+  }
 }
--- src/app/models/statistique.ts
+++ src/app/models/statistique.ts
@@ -1,5 +1,11 @@
-export type Statistique = {
-    id : string,
-    titre : string,
-    valeur : string
+export class Statistique {
+    id : string;
+    titre : string;
+    valeur : string;
+
+    constructor(id: string, titre: string, valeur: string){
+      this.id = id;
+      this.titre = titre;
+      this.valeur = valeur;
+    }
   }
